engstk/op7

input: touchscreen: add gestures haptic feedback control
echo 'Y|N' > /sys/module/touchpanel_common_driver/parameters/haptic_feedback_disable

Signed-off-by: engstk <eng.stk@sapo.pt>

drivers/vibrator/aw8697.c
@@ -77,6 +77,15 @@ struct pm_qos_request pm_qos_req_vb;
 /* add haptic audio tp mask */
 extern struct shake_point record_point[10];
 /* add haptic audio tp mask end */
+
+#ifdef CONFIG_HAPTIC_FEEDBACK_DISABLE
+int ignore_next_request = 0;
+void hap_ignore_next_request(void)
+{
+    ignore_next_request = 1;
+}
+#endif
+
 /******************************************************
  *
  * variable
@@ -781,6 +790,13 @@ static int aw8697_haptic_set_bst_peak_cur(struct aw8697 *aw8697, unsigned char p
 static unsigned char aw8697_haptic_set_level(struct aw8697 *aw8697, int gain)
 {
     int val = 80;
+
+#ifdef CONFIG_HAPTIC_FEEDBACK_DISABLE
+    if ((ignore_next_request) && (val != 0)) {
+       ignore_next_request = 0;
+       return 0;
+    }
+#endif
 
     val = aw8697->level * gain / 3;
     if (val > 255)

Bettehem/android_kernel_oneplus_sm7250-halium

Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

drivers/oneplus/vibrator/aw8697.c
@@ -126,6 +126,15 @@ struct pm_qos_request pm_qos_req_vb;
 /* add haptic audio tp mask */
 //extern struct shake_point record_point[10];
 /* add haptic audio tp mask end */
+
+#ifdef CONFIG_HAPTIC_FEEDBACK_DISABLE
+int ignore_next_request = 0;
+void hap_ignore_next_request(void)
+{
+    ignore_next_request = 1;
+}
+#endif
+
 /******************************************************
  *
  * variable
@@ -921,6 +930,13 @@ static int aw8697_haptic_set_bst_peak_cur(struct aw8697 *aw8697, unsigned char p
 static unsigned char aw8697_haptic_set_level(struct aw8697 *aw8697, int gain)
 {
     int val = 80;
+
+#ifdef CONFIG_HAPTIC_FEEDBACK_DISABLE
+    if ((ignore_next_request) && (gain != 0)) {
+       ignore_next_request = 0;
+       return 0;
+    }
+#endif
 
     val = aw8697->level * gain / 3;
     if (val > 255)